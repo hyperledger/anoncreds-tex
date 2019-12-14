b6f9984f9aa0efa8abb70bae86f9459c320cc7c6
Ry Jones <ry@linux.com>
2019-12-13 16:53:31 -0800

    Migrate to AZP
    Signed-off-by: Ry Jones <ry@linux.com>

--- check.sh
+++ check.sh
@@ -15,7 +15,7 @@ FAIL=0
 for f in $(find . -name main.tex | sed $FLAG 's|/[^/]+$||')
 do
     echo -e "${ESCAPE}${GREEN}Checking $f/main.tex${ESCAPE}${NC}"
-    docker run -v "$TRAVIS_BUILD_DIR/$f":/tex -w /tex dxjoke/tectonic-docker /bin/sh -c "tectonic main.tex"
+    docker run -v "$PWD/$f":/tex -w /tex dxjoke/tectonic-docker /bin/sh -c "tectonic main.tex"
     if [ $? -gt 0 ] ; then
         FAIL=1
     fi
